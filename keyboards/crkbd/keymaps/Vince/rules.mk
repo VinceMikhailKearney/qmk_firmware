f85a6e387d45d7b5262c14f7e18035cdc052e951
Vince <vince.kearney@onfido.com>
2024-09-28 22:46:24 +0200

    Disable tapping term, not used anymore

--- keyboards/crkbd/keymaps/Vince/rules.mk
+++ keyboards/crkbd/keymaps/Vince/rules.mk
@@ -6,7 +6,7 @@ OLED_ENABLE     = no
 LTO_ENABLE      = yes
 
 # Allows quantum keys to configure the tapping term via the keyboard
-DYNAMIC_TAPPING_TERM_ENABLE = yes
+# DYNAMIC_TAPPING_TERM_ENABLE = yes
 
 SEND_STRING_ENABLE = yes
 PROGRAMMABLE_BUTTON_ENABLE = yes
